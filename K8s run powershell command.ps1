9c8e5f6706574aa65ce92aa5482aa7dfedd031b2
Chukwunonso Nnamani <nomsynonso@hotmail.com>
2021-11-25 00:47:03 +0000

    update

--- K8s run powershell command.ps1
+++ K8s run powershell command.ps1
@@ -1,9 +1,9 @@
 kubectl run nginx `
---image=nginx `
---port=8080 `
---env="env=DEV" `
---labels="app=nginx,owner=user" `
---privileged=false `
---dry-run=client `
---output=yaml | `
-Out-File '.\nginx-pod.yaml'
+    --image=nginx `
+    --port=8080 `
+    --env="env=DEV" `
+    --labels="app=nginx,owner=user" `
+    --privileged=false `
+    --dry-run=client `
+    --output=yaml | `
+    Out-File '.\nginx-pod.yaml'
